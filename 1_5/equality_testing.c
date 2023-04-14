f5b26a035cf17539f9e7b534b34aa3997796f5c0
KingZuko <rs6121@nyu.edu>
2023-04-14 04:24:05 -0400

    added 1_9

--- 1_5/equality_testing.c
+++ 1_5/equality_testing.c
@@ -2,12 +2,15 @@
 
 int main(){
     int c;
+    int ndigit[10];
     while ((c = getchar()) != EOF){
-        if (c == 'a'){
-            printf("the lowercase a is in this text file");
-            // immediately terminate
-            return 0;
+        if (c >= '0' && c <= '9'){
+            ndigit[c-'0']++;
+            putchar(c);
         }
+    }
+    for (int i =0; i<10; i++){
+        printf("There are %d %d's\n",ndigit[i],i);
     }
     return 0;
 
